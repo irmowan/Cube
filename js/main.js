a999c181bbb9f57d305c143feb3f2d830fd047e3
irmo <irmo@irmodeMBP.lan>
2016-11-06 01:20:25 +0800

    Set cube color

--- js/main.js
+++ js/main.js
@@ -2,16 +2,32 @@
  * Created by irmo on 16/11/5.
  */
 var scene, camera, renderer;
-var geometry, material, mesh;
+var geometry, material, cube;
 
-function init() {
+var colors = [
+  0xffffff,
+  0xff0000,
+  0xffff00,
+  0x0000ff,
+  0xffa500,
+  0x00ff00
+];
+
+function init(dimensions) {
+  var d = dimensions || 3;
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
-  camera.position.z = 1000;
-  geometry = new THREE.BoxGeometry(200, 200, 200);
-  material = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
-  mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
+  camera.position.z = 500;
+  geometry = new THREE.BoxGeometry(200, 200, 200, d, d, d);
+  for (var i = 0; i < 6; ++i) {
+    for (var j = 0; j < 2 * d * d; ++j) {
+      geometry.faces[(2 * d * d) * i + j].color.setHex(colors[i]);
+    }
+  }
+  material = new THREE.MeshBasicMaterial({color: 0xffffff, vertexColors: THREE.FaceColors, wireframe: true});
+  cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
@@ -19,8 +35,8 @@ function init() {
 
 function animate() {
   requestAnimationFrame(animate);
-  mesh.rotation.x += 0.01;
-  mesh.rotation.y += 0.02;
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
   renderer.render(scene, camera);
 }
 
